3a3538d9e78e407dc25e17ede2db72deb002c66b
Michael Lesniak <mail@mlesniak.com>
2021-03-03 22:30:21 +0100

    Show printable characters

--- src/main/kotlin/com/mlesniak/jvm/Utils.kt
+++ src/main/kotlin/com/mlesniak/jvm/Utils.kt
@@ -6,24 +6,34 @@ object Utils {
      **/
     fun printBytes(bytes: ByteArray) {
         var ip = 0x00
-        var sb = StringBuilder()
+        var hex = StringBuilder()
+        var values = StringBuilder()
+
         for (b in bytes) {
             if (ip % 8 == 0) {
-                sb.clear()
-                sb.append(String.format("%04X\t", ip))
+                hex.clear()
+                values.clear()
+                hex.append(String.format("%04X\t", ip))
             }
 
             val s = String.format("%02X ", b)
-            sb.append(s)
+            hex.append(s)
+
+            var bval = '.'
+            if (b > 31) {
+                bval = b.toChar()
+            }
+            values.append(bval)
+
             ip++
 
             if (ip % 8 == 0) {
-                println(sb.toString())
+                println(hex.toString() + "\t" + values.toString())
             }
         }
 
-        if (sb.isNotEmpty()) {
-            println(sb.toString())
+        if (hex.isNotEmpty()) {
+            println(hex.toString())
         }
     }
 }
